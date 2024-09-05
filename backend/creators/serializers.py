a195c60b75e682f099e40d13ef09fd8fdae33c40
John Doe <jjhu1999@gmail.com>
2024-09-05 13:19:48 -0700

    let's go

--- backend/creators/serializers.py
+++ backend/creators/serializers.py
@@ -4,5 +4,5 @@ from .models import Creators
 class CreatorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Creators
-        # fields = ('id', 'created_at', 'name', 'url', 'description', 'imageurl')
-        fields = '__all__'
+        fields = ('created_at', 'name', 'url', 'description', 'imageurl')
+        # fields = '__all__'
