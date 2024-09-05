5f0b8bdd07a41a2c500ceeb1ba97008b4d828c37
John Doe <jjhu1999@gmail.com>
2024-09-04 18:32:14 -0700

    please work fuck

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'corsheaders.middleware.CorsPostCsrfMiddleware'
 ]
 
 ROOT_URLCONF = "backend.urls"
