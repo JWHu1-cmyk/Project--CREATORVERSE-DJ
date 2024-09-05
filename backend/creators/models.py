a507eb394ea67b880710a0abccc52527e5a31f93
John Doe <jjhu1999@gmail.com>
2024-09-05 03:45:08 -0700

    pleae

--- backend/creators/models.py
+++ backend/creators/models.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 # Create your models here.
 
 class Creators(models.Model):
-    id = models.AutoField(primary_key=True)
+    # id = models.AutoField(primary_key=True)
     # id = models.IntegerField(primary_key=True)
     # created_at = models.DateField(auto_now_add=True)
-    created_at = models.TextField(editable=False)
+    created_at = models.TextField()
     name = models.TextField()
     url = models.TextField()
     description = models.TextField()
@@ -18,9 +18,4 @@ class Creators(models.Model):
     def _str_(self):
         return self.title
     
-    def save(self, *args, **kwargs):
-        if not self.id:
-            now = timezone.now()
-            self.created_at = now.strftime("%Y-%m-%d %H:%M:%S")
-            self.created_at_date = now
-        super(Creators, self).save(*args, **kwargs)
+ 
