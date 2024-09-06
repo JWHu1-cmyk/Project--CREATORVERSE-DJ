5f30dd98edabc298fa3672a430ced5452faa5265
John Doe <jjhu1999@gmail.com>
2024-09-06 11:52:44 -0700

    almost

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -140,6 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
