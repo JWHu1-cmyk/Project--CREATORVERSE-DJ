39b715980448051a75fd7f3fde51f83898c379dd
John Doe <jjhu1999@gmail.com>
2024-09-05 22:38:30 -0700

    wooo

--- backend/creators/models.py
+++ backend/creators/models.py
@@ -6,7 +6,7 @@ class Creators(models.Model):
     # id = models.AutoField(primary_key=True)
     # id = models.IntegerField(primary_key=True)
     # created_at = models.DateField(auto_now_add=True)
-    creator_id = models.AutoField(primary_key=True, serialize=True)
+    id = models.AutoField(primary_key=True, serialize=True)
     created_at = models.TextField()
     name = models.TextField()
     url = models.TextField()
