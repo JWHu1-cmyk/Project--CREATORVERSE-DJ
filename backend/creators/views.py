897aa99f979085f914707642fb8d711f68676da6
John Doe <jjhu1999@gmail.com>
2024-09-07 02:26:28 -0700

    pleae

--- backend/creators/views.py
+++ backend/creators/views.py
@@ -8,6 +8,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 # Create your views here.
 
 class CreatorsView(viewsets.ModelViewSet):
@@ -18,3 +21,11 @@ class CreatorsView(viewsets.ModelViewSet):
     queryset = Creators.objects.all()
     
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    
+    @method_decorator(cache_page(60 * 30))  # Cache GET requests for 30 minutes
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 30))  # Cache retrieve requests for 30 minutes
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
