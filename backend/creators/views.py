b02285170e043044fdd38ddaeecb7d07d580be15
John Doe <jjhu1999@gmail.com>
2024-09-07 04:35:36 -0700

    let's go

--- backend/creators/views.py
+++ backend/creators/views.py
@@ -11,6 +11,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 
+from django.core.cache import cache
+
 # Create your views here.
 
 class CreatorsView(viewsets.ModelViewSet):
@@ -22,10 +24,29 @@ class CreatorsView(viewsets.ModelViewSet):
     
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     
-    @method_decorator(cache_page(60 * 30))  # Cache GET requests for 30 minutes
+    # @method_decorator(cache_page(60 * 30))  # Cache GET requests for 30 minutes
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
-    @method_decorator(cache_page(60 * 30))  # Cache retrieve requests for 30 minutes
+    # @method_decorator(cache_page(60 * 30))  # Cache retrieve requests for 30 minutes
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        result = super().perform_create(serializer)
+        self.invalidate_cache()
+        return result
+
+    def perform_update(self, serializer):
+        result = super().perform_update(serializer)
+        self.invalidate_cache()
+        return result
+
+    def perform_destroy(self, instance):
+        result = super().perform_destroy(instance)
+        self.invalidate_cache()
+        return result
+
+    def invalidate_cache(self):
+        cache.clear()  # Clear entire cache
+        # Or use more specific cache key invalidation if possible
