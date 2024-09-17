88f84e6bec3ea3f1ba22facb5ae6d6561ff1656e
John Doe <jjhu1999@gmail.com>
2024-09-17 02:55:55 -0700

    please

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 
 export default function Root() {
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const query = event.target.elements.q.value;
+    navigate(`/showCreators?q=${query}`);
+  };
+
   return (
     <div className="container mt-5">
  
       <h1 className="text-center mb-4">CREATORVERSE</h1>
       <div className="d-flex flex-column align-items-center mb-4">
-        <div className="d-flex justify-content-center mb-4" style={{ width: '100%', maxWidth: '300px' }}>
+        <form onSubmit={handleSubmit} className="d-flex justify-content-center mb-4" style={{ width: '100%', maxWidth: '300px' }}>
+          {/* <div className="d-flex justify-content-center mb-4" style={{ width: '100%', maxWidth: '300px' }}> */}
           <input
             type="text"
+            name="q"
             className="form-control"
             placeholder="Search creators..."
             style={{ marginRight: '10px' }}
@@ -17,7 +26,8 @@ export default function Root() {
           <button type="button" className="btn btn-dark">
               SEARCH
           </button>
-        </div>
+          {/* </div> */}
+        </form>
         <Link to="showCreators" className="mb-3 w-100" style={{ maxWidth: '300px' }}>
           <button type="button" className="btn btn-dark w-100 px-4 py-2">
             VIEW ALL CREATORS
