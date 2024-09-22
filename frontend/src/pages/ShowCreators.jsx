c29ce6a923e515e9521d130536ee7fa41640df35
John Doe <jjhu1999@gmail.com>
2024-09-22 00:30:10 -0700

    hta'

--- frontend/src/pages/ShowCreators.jsx
+++ frontend/src/pages/ShowCreators.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Outlet, useLocation, useLoaderData } from 'react-router-dom';
+import { Outlet, useLocation, useLoaderData, useNavigate } from 'react-router-dom';
 import { getCreators } from '../Creator1';
 import CreatorListItem from './CreatorListItem';
 import axios from 'axios';
@@ -26,36 +26,46 @@ export default function ShowCreators() {
   const location = useLocation();
   const query = new URLSearchParams(location.search).get('q');
   const loaderData = useLoaderData();
-
+  const navigate = useNavigate();
+  
+  // useEffect(() => {
+  //   if (query) {
+  //     const encodedQuery = encodeURIComponent(query);
+  //     axios.get(`${API_URL}search?q=${encodedQuery}/`, {
+  //       withCredentials: true,
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'X-Requested-With': 'XMLHttpRequest',
+  //       }
+  //     })
+  //     .then(response => {
+  //       console.log('Raw response:', response);
+  //       setResults(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching search results:', error);
+  //       if (error.response) {
+  //         console.log('Response data:', error.response.data);
+  //         console.log('Response status:', error.response.status);
+  //         console.log('Response headers:', error.response.headers);
+  //       }
+  //     });
+  //   } else {
+  //     setContacts(loaderData.contacts);
+  //   }
+  // }, [query, loaderData.contacts]);
+  // // axios.get('/api/creatorss/')
+  // // axios.get(`/search?q=${query}/`)
   useEffect(() => {
     if (query) {
       const encodedQuery = encodeURIComponent(query);
-      axios.get(`${API_URL}search?q=${encodedQuery}/`, {
-        withCredentials: true,
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Requested-With': 'XMLHttpRequest',
-        }
-      })
-      .then(response => {
-        console.log('Raw response:', response);
-        setResults(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching search results:', error);
-        if (error.response) {
-          console.log('Response data:', error.response.data);
-          console.log('Response status:', error.response.status);
-          console.log('Response headers:', error.response.headers);
-        }
-      });
+      const searchUrl = `${API_URL}search?q=${encodedQuery}/`;
+      window.location.href = searchUrl;
     } else {
       setContacts(loaderData.contacts);
     }
   }, [query, loaderData.contacts]);
-  // axios.get('/api/creatorss/')
-  // axios.get(`/search?q=${query}/`)
-
+  
   const itemsToDisplay = query ? results : contacts;
   console.log('itemsToDisplay:', itemsToDisplay);
 
